ea3acd9f98e81c8b86f5bda205f7c5fb3dc4fd98
Duong Nguyen <duongital@gmail.com>
2022-02-25 10:17:22 +0700

    fix: drag vs click events

--- src/components/Sliders.js
+++ src/components/Sliders.js
@@ -1,6 +1,6 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 import gsap from 'gsap'
 
 import Cover1 from 'assets/img/cover1.jpg'
@@ -11,12 +11,11 @@ import Cover5 from 'assets/img/cover5.jpg'
 import Cover6 from 'assets/img/cover6.jpg'
 
 export default function Sliders() {
+  const router = useRouter()
   useEffect(async () => {
     const Flickity = await import('flickity/dist/flickity.pkgd')
-    console.log(Flickity)
     const elem = document.querySelector('.main-carousel')
     const flkty = new Flickity.default(elem, {
-      // options
       cellAlign: 'left',
       contain: true,
       height: '160px',
@@ -26,83 +25,44 @@ export default function Sliders() {
       pageDots: false,
     })
 
-    // $(".main-carousel").flickity({
-    //   // options
-    //   cellAlign: "left",
-    //   contain: true,
-    // });
+    flkty.on('pointerDown', function () {
+      setIsDragging(false)
+    })
+
+    flkty.on( 'pointerMove', function() {
+      setIsDragging(true)
+    });
   }, [])
 
   const belowText = useRef()
+  const [isDragging, setIsDragging] = useState(false)
   useEffect(() => {
     gsap.fromTo(belowText.current, { y: 50 }, { duration: 0.5, y: 0 })
   })
 
   return (
-    <div className="sliders-wrapper">
+    <div className="mt-8 sliders-wrapper">
       <div className="main-carousel">
-        <Link href="/work/1" className="carousel-cell mx-4">
-          <div>
-            <Image
-              height={400}
-              width={500}
-              placeholder="blur"
-              src={Cover1}
-              alt="cover 1"
-            />
-            <h1 ref={belowText}>below text</h1>
-          </div>
-        </Link>
-        <div className="carousel-cell mx-">
-          <Image
-            height={400}
-            width={500}
-            placeholder="blur"
-            src={Cover2}
-            alt="cover 2"
-          />
-          <h1 ref={belowText}>below text</h1>
-        </div>
-        <div className="carousel-cell mx-">
-          <Image
-            height={400}
-            width={500}
-            placeholder="blur"
-            src={Cover3}
-            alt="cover 3"
-          />
-          <h1 ref={belowText}>below text</h1>
-        </div>
-        <div className="carousel-cell mx-">
-          <Image
-            height={400}
-            width={500}
-            placeholder="blur"
-            src={Cover4}
-            alt="cover 4"
-          />
-          <h1 ref={belowText}>below text</h1>
-        </div>
-        <div className="carousel-cell mx-">
-          <Image
-            height={400}
-            width={500}
-            placeholder="blur"
-            src={Cover5}
-            alt="cover 5"
-          />
-          <h1 ref={belowText}>below text</h1>
-        </div>
-        <div className="carousel-cell mx-">
-          <Image
-            height={400}
-            width={500}
-            placeholder="blur"
-            src={Cover6}
-            alt="cover 6"
-          />
-          <h1 ref={belowText}>below text</h1>
-        </div>
+        {[Cover1, Cover2, Cover3, Cover4, Cover5, Cover6].map((e) => {
+          return (
+            <div
+              className="carousel-cell"
+              key={e}
+              onClick={(event) => {
+                console.log("i am click or drag: ", isDragging)
+                if (!isDragging) {
+                  router.push('/work/1')
+                }
+              }}
+            >
+              <Image height={400} width={500} src={e} alt="cover 1" />
+              <div className="flex justify-between items-center">
+                <h4>Is the commit</h4>
+                <h5>Towl Hall</h5>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )

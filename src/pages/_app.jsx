db3d5914cc9c0e1c84591c418a14128ac71909cd
Duong Nguyen <duongital@gmail.com>
2022-03-06 13:50:18 +0700

    fix: load lib first (2)

---
+++ src/pages/_app.jsx
@@ -0,0 +1,13 @@
+import 'styles/globals.css'
+import 'flickity/dist/flickity.min.css'
+
+import { useEffect } from 'react'
+
+function MyApp({ Component, pageProps }) {
+  useEffect(async () => {
+    await import('jquery')
+    await import('flickity/dist/flickity.pkgd')
+  }, [])
+  return <Component {...pageProps} />
+}
+export default MyApp

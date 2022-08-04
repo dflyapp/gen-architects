5fe57999ff2c2492603af56171d3687fcc7d94be
Duong Nguyen <duongital@gmail.com>
2022-08-04 16:38:21 +0700

    feat: add gtag manager

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 import 'styles/globals.css'
 import 'flickity/dist/flickity.min.css'
 
@@ -8,7 +10,24 @@ function MyApp({ Component, pageProps }) {
     await import('jquery')
     await import('flickity/dist/flickity.pkgd')
   }, [])
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-P2Z499R4XQ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-P2Z499R4XQ');
+  `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp

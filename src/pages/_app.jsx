95421800c71b5cf321f17135d659420b3fcdaa9f
duongital <duongital@gmail.com>
2024-08-27 10:55:07 +0700

    chore: revert GA

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,18 @@ import 'styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-P2Z499R4XQ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-P2Z499R4XQ');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   )
